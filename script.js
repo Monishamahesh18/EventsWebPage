53429bd7913a2ebaa0039f4ed639ce286ef1f369
Monishamahesh18 <monishamaheshkarnam@gmail.com>
2025-12-03 16:47:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scroll
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
